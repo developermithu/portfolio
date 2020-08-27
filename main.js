5171166aab09e8fcf80bc154a4c1413307720aca
Mithu105 <www.mithudas77@gmail.com>
2020-08-27 16:20:57 +0600

    added

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+  //=======  Mobile Menu Button  ======//
+
   //=======  Typed Js  ======//
   var typed = new Typed("#typed", {
     strings: [
@@ -13,6 +15,39 @@ $(document).ready(function () {
   });
 
   //=======  Counter Up  ======//
-  $(".number").counterUp({ delay: 10, time: 3000 });
+  $(".number").counterUp({ delay: 10, time: 2000 });
 
+  //=======  Counter Up  ======//
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $(".scroll-up-btn").fadeIn();
+    } else {
+      $(".scroll-up-btn").fadeOut();
+    }
+  });
+
+  $(".scroll-up-btn").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 500);
+  });
+
+  //=======  Counter Up  ======//
+  AOS.init();
+});
+
+// =======  smooth scroll for IE / Safari / Ege ========//
+
+$("a").on("click", function (event) {
+  if (this.hash != "") {
+    event.preventDefault();
+    var hash = this.hash;
+    $("html,body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      500,
+      function () {
+        window.location.hash = hash;
+      }
+    );
+  }
 });
